fbcdd0e007aa9d8c7c8fad078cba52a4cf0e6ba2
Nyirabazungu <Rutagarama1>
2019-01-19 11:26:45 +0200

    add function in js

---
+++ js/script.js
@@ -0,0 +1,27 @@
+// business logic
+function account(first, last) {
+    this.firstName = first;
+    this.lastName = last;
+    this.Initialamout=Initialamout
+    this.deposit= deposit;
+    this.withdraw=withdraw;
+    totalamount=Initialamout+this.deposit
+    totalbalance=totalamount-this.withdraw 
+  }
+  
+  // user interface logic
+  $(document).ready(function() {
+    $("form#new-contact").submit(function(event) {
+      event.preventDefault();
+  
+      var inputtedFirstName = $("input#new-first-name").val();
+      var inputtedLastName = $("input#new-last-name").val();
+  
+      var newContact = new Contact(inputtedFirstName, inputtedLastName);
+  
+      $("ul#contacts").text("<li><span class='account'>" + newContact.firstName + "</span></li>");
+  
+      $("input#new-first-name").val("");
+      $("input#new-last-name").val("");
+    });
+  }); 
